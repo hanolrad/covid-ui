fdd59bc1fbb0cdf0b34b2db6048e381bf7d4d3bc
Radek <radek.hanold.jr@gmail.com>
2023-08-16 16:36:39 +0200

    deps & query context

---
+++ pages/client-only.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ClientOnly = () => {
+  return (
+    <p>This data is only available on the client and not prefetched.
+    </p>
+  )
+}
+
+export default ClientOnly
---
+++ src/utils/provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      cacheTime: 1000 * 60 * 30, // 30 minutes
+      retry: 1,
+    },
+  },
+});
+
+function Providers({ children }: React.PropsWithChildren) {
+  const [client] = React.useState(queryClient);
+
+  return (
+    <QueryClientProvider client={client}>
+        {children}
+    </QueryClientProvider>
+  );
+}
+
+export default Providers;
